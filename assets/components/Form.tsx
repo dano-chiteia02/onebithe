7197a81a241e9c42c439f200ab1cc3e43c1af898
dano-chiteia02 <157301664+dano-chiteia02@users.noreply.github.com>
2025-12-02 19:30:39 +0100

    nop

--- assets/components/Form.tsx
+++ assets/components/Form.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, TextInput, View, Image} from 'react-native';
 export default function App() {
   return (
     <View >
-      <Text>form components</Text>
+      <Text>form components ffff</Text>
         <TextInput placeholder='Agora sim faz sentido eu estar a usar o text input ' />
     </View>
   );
