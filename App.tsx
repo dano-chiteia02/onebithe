ed3848a7ad56ae5ac63bc11a68de401e569209d2
dano-chiteia02 <157301664+dano-chiteia02@users.noreply.github.com>
2025-12-02 19:28:45 +0100

    Its my first app in react native

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View} from 'react-native';
+import Form from './assets/components/Form';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View>
+       
+      </View>
+      
+
+      <Text>Its de first time whrinting in agora colou  react native ap</Text>
+       <Form />
+      <TextInput placeholder='sera que assim' />
+      <TextInput placeholder='sera que assim' keyboardType='numeric' />
       <StatusBar style="auto" />
     </View>
   );
---
+++ assets/components/Form.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, TextInput, View, Image} from 'react-native';
+
+
+export default function App() {
+  return (
+    <View >
+      <Text>form components</Text>
+        <TextInput placeholder='Agora sim faz sentido eu estar a usar o text input ' />
+    </View>
+  );
+}
